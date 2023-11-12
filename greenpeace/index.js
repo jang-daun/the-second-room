1ab9642e0a39338093586c26598ed4847f0b0a24
jang-daun <moone0327@gmail.com>
2023-11-12 17:12:54 +0900

    .

---
+++ greenpeace/index.js
@@ -0,0 +1,18 @@
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $("#MOVE_TOP_BTN").fadeIn();
+    } else {
+      $("#MOVE_TOP_BTN").fadeOut();
+    }
+  });
+  $("#MOVE_TOP_BTN").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
